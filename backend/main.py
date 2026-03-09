27499f95cc22af92c60234b3abb86226c5cc65fa
Suraj Singh <165886618+singhsrj@users.noreply.github.com>
2026-03-09 15:57:40 +0530

    Change CORS settings to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="WatchParty API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://watchpartyt.netlify.app/","http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
